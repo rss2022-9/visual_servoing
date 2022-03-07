4a7097662dd79de28e806bd506dd46f9eb45bf9e
Meenakshi Singh <mrsingh0306@gmail.com>
2022-03-06 21:24:08 -0500

    cone detector color filter

--- scripts/computer_vision/color_segmentation.py
+++ scripts/computer_vision/color_segmentation.py
@@ -34,8 +34,29 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
+	hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
-	bounding_box = ((0,0),(0,0))
+	lower_orange = np.array([20,50,50])
+	upper_orange = np.array([40,255,255])
+	orange_filter = cv2.inRange(img,lower_orange, upper_orange)
+	cv2.imshow('orange filter',orange_filter)
+
+	#opening: erosion followed by dilation
+	kernel = np.ones(5,5)
+	opening = cv2.morphologyEx(orange_filter, cv2.MORPH_OPEN, kernel)
+	cv2.imshow('opening',opening)
+
+
+	# Bitwise-AND mask and original image, note, rn not using the opening at all
+	res = cv2.bitwise_and(image,image, mask= orange_filter)
+	cv2.imshow('res',res)
+	contours,hierarchy = cv2.findContours(res,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+	contours.sort(key = cv2.contourArea, reverse = True)
+
+	#top left coordinate of rectangle is (x,y)
+	x,y,w,h = cv2.boundingRect(contours[0])
+
+	bounding_box = ((x,y),(x+w,y+h))
 
 	########### YOUR CODE ENDS HERE ###########
 
